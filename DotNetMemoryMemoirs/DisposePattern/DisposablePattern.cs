dc569646ecb0c1db9b4b77773765533416b32c88
Pieter Engelbrecht <pgengel@gmail.com>
2017-07-01 17:06:46 +0200

    Added the dispose pattern and weal references.

---
+++ DotNetMemoryMemoirs/DisposePattern/DisposablePattern.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetMemoryMemoirs.DisposePattern
+{
+	class DisposablePattern : IDisposable
+	{
+		private FileStream _fileStream;
+		private IntPtr _handle = Marshal.AllocHGlobal(4);
+
+		public DisposablePattern(FileStream fileStream)
+		{
+			_fileStream = fileStream;
+		}
+
+		/// <summary>
+		/// Dispose managed resources
+		/// </summary>
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// When finalizing (GC), destroy unmanaged resources
+		/// </summary>
+		~DisposablePattern()
+		{
+			Dispose(false);
+		}
+
+		/// <summary>
+		/// Actual dispose
+		/// </summary>
+		/// <param name="destroyManaged">Should dispose managed objects?</param>
+		private void Dispose(bool destroyManaged)
+		{
+			if (destroyManaged)
+			{
+				_fileStream.Dispose();
+			}
+
+			Marshal.FreeHGlobal(_handle);
+		}
+	}
+}
---
+++ DotNetMemoryMemoirs/WeakReferences/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetMemoryMemoirs.WeakReferences
+{
+	class Data
+	{
+		private byte[] _data;
+		private string _name;
+
+		public Data(int size)
+		{
+			_data = new byte[size * 1024];
+			_name = size.ToString();
+		}
+
+		// Simple property.
+		public string Name
+		{
+			get { return _name; }
+		}
+	}
+}
