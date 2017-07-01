b4ae271c089d0a33b15f367d1c8675faf397b429
Pieter Engelbrecht <pgengel@gmail.com>
2017-07-01 14:56:18 +0200

    String Allocation

--- DotNetMemoryMemoirs/Program.cs
+++ DotNetMemoryMemoirs/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DotNetMemoryMemoirs.StringsAllocations;
 
 namespace DotNetMemoryMemoirs
 {
@@ -10,6 +11,66 @@ namespace DotNetMemoryMemoirs
 	{
 		static void Main(string[] args)
 		{
+			var demosToRun = new List<IDemo>();
+
+			Console.Write("Which demo would you like to run? (enter a number) ");
+			var demoNumber = Console.ReadLine().TrimEnd('\r', 'n');
+
+			switch (demoNumber.ToLowerInvariant())
+			{
+				case "1":
+					//demosToRun.Add(new WeakReferenceDemo());
+					//demosToRun.Add(new DisposeObjectsDemo());
+					break;
+
+				case "1-1":
+					//demosToRun.Add(new WeakReferenceDemo());
+					break;
+
+				case "1-2":
+					//demosToRun.Add(new DisposeObjectsDemo());
+					break;
+
+				case "3":
+					//demosToRun.Add(new BeersDemoUnoptimized());
+					//demosToRun.Add(new BeersDemoOptimized());
+					break;
+
+				case "3-1":
+					//demosToRun.Add(new BeersDemoUnoptimized());
+					break;
+
+				case "3-2":
+					//demosToRun.Add(new BeersDemoOptimized());
+					break;
+
+				case "4":
+				case "4-1":
+					demosToRun.Add(new StringAllocation());
+					break;
+
+				default:
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine("Unknown demo. Try restarting the application.");
+					Console.ResetColor();
+					Console.ReadLine();
+					break;
+			}
+
+			foreach (var demo in demosToRun)
+			{
+				GC.Collect();
+				Console.Clear();
+
+				demo.Run(args);
+
+				Console.WriteLine();
+				Console.ResetColor();
+				Console.ForegroundColor = ConsoleColor.DarkGray;
+				Console.WriteLine("Press <enter> to continue.");
+				Console.ResetColor();
+				Console.ReadLine();
+			}
 		}
 	}
 }
